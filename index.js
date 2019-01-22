34029cd6da929c3b5e4e313aa5fe64068b2be156
Lucifer <ramanandsirvi21@gmail.com>
2019-01-22 14:14:16 +0530

    Basic Bot Setup Done

---
+++ index.js
@@ -0,0 +1,24 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame("on SourceCode!");
+});
+
+client.on("message", msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = msg.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}hello`) {
+    return msg.channel.send("Hello!");
+  }
+});
+
+client.login(botconfig.token);
